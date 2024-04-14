2f067d641e8faba47a3e5b1934db0d3dded6e695
Pulni <pulni@pulni.com>
2024-04-14 18:54:50 +0300

    Stuff? :D

--- Assets/Scripts/SummonInfoButton.cs
+++ Assets/Scripts/SummonInfoButton.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class SummonInfoButton : MonoBehaviour {
 	[SerializeField] private Mob mob;
 	[SerializeField] private Button button;
+	[SerializeField] private Image buttonBackground;
 	[SerializeField] private TMP_Text mobName;
+	[SerializeField] private InputActionReference switchAction;
 
 	private void Start() {
 		this.button.onClick.AddListener(this.SelectMob);
@@ -19,7 +22,17 @@ public class SummonInfoButton : MonoBehaviour {
 		this.button.onClick.RemoveListener(this.SelectMob);
 	}
 
+	private void Update() {
+		if (this.switchAction.action.WasPerformedThisFrame()) {
+			this.SelectMob();
+		}
+	}
+
 	private void SelectMob() {
 		GameManager.Instance.player.MobToSummon = this.mob;
+		foreach (var btn in GameObject.FindObjectsOfType<SummonInfoButton>()) {
+			btn.buttonBackground.color = Color.white;
+		}
+		this.buttonBackground.color = Color.yellow;
 	}
 }
